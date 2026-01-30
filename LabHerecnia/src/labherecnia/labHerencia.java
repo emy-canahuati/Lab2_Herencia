612926742630c2a2a39d79c9cda7a266f5b6f170
AdanPalma18 <palmaadan54@gmail.com>
2026-01-30 11:20:23 -0500

    Bases GUI

---
+++ LabHerecnia/src/labherecnia/labHerencia.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labherecnia;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author adria
+ */
+public class labHerencia extends JFrame {
+
+    public JPanel panelBotones;
+    public JPanel panelCentral;
+
+    public CardLayout cardLayout;
+
+
+
+    public labHerencia() {
+        setTitle("Gestion Empleados");
+        setSize(800, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        panelBotones = new JPanel();
+        panelBotones.setLayout(new GridLayout(0, 1, 5, 5));
+        panelBotones.setBorder(BorderFactory.createTitledBorder("Menú"));
+        JButton btnAgregarEstandar = new JButton("Agregar Empleado Estándar");
+        JButton btnAgregarTemporal = new JButton("Agregar Empleado Temporal");
+        JButton btnAgregarVentas = new JButton("Agregar Empleado Ventas");
+        JButton btnRegistrarHoras = new JButton("Registrar Horas");
+
+
+        panelBotones.add(btnAgregarEstandar);
+        panelBotones.add(btnAgregarTemporal);
+        panelBotones.add(btnAgregarVentas);
+        panelBotones.add(btnRegistrarHoras);
+
+
+        add(panelBotones, BorderLayout.WEST);
+
+        cardLayout = new CardLayout();
+        panelCentral = new JPanel(cardLayout);
+
+        JPanel panelEstandar = new JPanel();
+        panelEstandar.add(new JLabel("Formulario Empleado Estándar"));
+
+        JPanel panelTemporal = new JPanel();
+        panelTemporal.add(new JLabel("Formulario Empleado Temporal"));
+
+        JPanel panelVentas = new JPanel();
+        panelVentas.add(new JLabel("Formulario Empleado Ventas"));
+
+        JPanel panelHoras = new JPanel();
+        panelHoras.add(new JLabel("Registrar Horas Trabajadas"));
+
+        panelCentral.add(panelEstandar, "ESTANDAR");
+        panelCentral.add(panelTemporal,"TEMPORAL");
+        panelCentral.add(panelVentas, "VENTAS");
+        panelCentral.add(panelHoras, "HORAS");
+
+        add(panelCentral, BorderLayout.CENTER);
+
+
+        btnAgregarEstandar.addActionListener(e -> cardLayout.show(panelCentral, "ESTANDAR"));
+        btnAgregarTemporal.addActionListener(e -> cardLayout.show(panelCentral, "TEMPORAL"));
+        btnAgregarVentas.addActionListener(e -> cardLayout.show(panelCentral, "VENTAS"));
+        btnRegistrarHoras.addActionListener(e -> cardLayout.show(panelCentral, "HORAS"));
+
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new labHerencia().setVisible(true);
+        });
+    }
+}
