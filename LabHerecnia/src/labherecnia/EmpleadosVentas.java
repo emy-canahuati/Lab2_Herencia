37284d22dae35894c2fd52fb25a8493115b9b30d
Emy Canahuati <emy_canahuati@unitec.edu>
2026-01-30 09:21:40 -0600

    EmpleadosVentas

---
+++ LabHerecnia/src/labherecnia/EmpleadosVentas.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labherecnia;
+
+
+
+/**
+ *
+ * @author emyca
+ */
+import java.time.LocalDateTime;
+public class EmpleadosVentas {
+    private double[] ventasMensuales;
+    private double tasaComision;
+    
+    public EmpleadosVentas(){
+        this.ventasMensuales= new double[12];
+    }
+    
+    public void registroVentas(int mes,double venta){
+        int mesActual = LocalDateTime.now().getMonthValue()-1;
+        ventasMensuales[mesActual]+=venta;
+    }
+    
+    public void setTasaComision(double tasaComision){
+        this.tasaComision=tasaComision;
+    }
+    
+    public double calculoComision(int mes){
+        return (ventasMensuales[mes]*tasaComision);
+    }
+    
+    
+    
+    public double ventasAnuales(){
+        
+    }
+}
